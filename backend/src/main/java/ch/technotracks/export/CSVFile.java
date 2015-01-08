3c97ec320fef94a499ad25769ac9b73612822142
EvelynHischier <evelyn_hischier@hotmail.com>
2015-01-08 22:42:01 +0100

    CSV export

--- backend/src/main/java/ch/technotracks/export/CSVFile.java
+++ backend/src/main/java/ch/technotracks/export/CSVFile.java
@@ -2,6 +2,8 @@ package ch.technotracks.export;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
 
 import ch.technotracks.backend.GPSData;
@@ -11,52 +13,48 @@ import ch.technotracks.backend.GPSData;
  */
 public class CSVFile {
 
-    private static void generateCSVFile(String fileName, List<GPSData> gpss) {
-        try {
-            FileWriter writer = new FileWriter(fileName);
+    public static String generateCSVFile(Collection<GPSData> gpss) {
+
+            StringBuffer buffer = new StringBuffer();
 
             // title
             // trackID, gpsID, accuracy, altitude, bearing, latitude, longitude, satellites, speed, timestamp
-            writer.append("trackID");
-            writer.append(',');
-            writer.append("gpsID");
-            writer.append(',');
-            writer.append("accuracy");
-            writer.append(',');
-            writer.append("altitude");
-            writer.append(',');
-            writer.append("bearing");
-            writer.append(',');
-            writer.append("latitude");
-            writer.append(',');
-            writer.append("longitude");
-            writer.append(',');
-            writer.append("satellites");
-            writer.append(',');
-            writer.append("speed");
-            writer.append(',');
-            writer.append("timestamp");
-            writer.append('\n');
+            buffer.append("trackID");
+            buffer.append(',');
+            buffer.append("gpsID");
+            buffer.append(',');
+            buffer.append("accuracy");
+            buffer.append(',');
+            buffer.append("altitude");
+            buffer.append(',');
+            buffer.append("bearing");
+            buffer.append(',');
+            buffer.append("latitude");
+            buffer.append(',');
+            buffer.append("longitude");
+            buffer.append(',');
+            buffer.append("satellites");
+            buffer.append(',');
+            buffer.append("speed");
+            buffer.append(',');
+            buffer.append("timestamp");
+            buffer.append("#");
 
-            // inserting data here
+            // appending data to string buffer
             for (GPSData gps : gpss){
 
-                writer.append(gps.getTrackID()+",");            // trackID,
-                writer.append(gps.getId()+",");                 // gpsID,
-                writer.append(gps.getAccuracy()+",");           // accuracy,
-                writer.append(gps.getAltitude()+",");           // altitude,
-                writer.append(gps.getBearing()+",");            // bearing,
-                writer.append(gps.getLatitude()+",");           // latitude,
-                writer.append(gps.getLongitude()+",");          // longitude,
-                writer.append(gps.getSatellites()+",");         // satellites,
-                writer.append(gps.getTimestamp().toString()+",");  // timestamp
-                writer.append('\n');                            // new line
+                buffer.append(gps.getTrackID()+",");            // trackID,
+                buffer.append(gps.getId()+",");                 // gpsID,
+                buffer.append(gps.getAccuracy()+",");           // accuracy,
+                buffer.append(gps.getAltitude()+",");           // altitude,
+                buffer.append(gps.getBearing()+",");            // bearing,
+                buffer.append(gps.getLatitude()+",");           // latitude,
+                buffer.append(gps.getLongitude()+",");          // longitude,
+                buffer.append(gps.getSatellites()+",");         // satellites,
+                buffer.append(gps.getSpeed()+",");              // speed
+                buffer.append(gps.getTimestamp().toString());  // timestamp
+                buffer.append("#");                            // new line
             }
-            // send data
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return buffer.toString();
     }
 }
---
+++ backend/src/main/webapp/js/exportCSV.js
@@ -0,0 +1,47 @@
+
+function CSVGenerator(content) {
+// inspired by http://www.hybridplanet.co.in/tutorial/javascript/how-to-create-csv-or-excel-file-from-json-via-javascript
+
+    var CSV = '';
+    var date = new Date();
+
+    CSV = content;
+
+    if (CSV == '') {
+        alert(content);
+        alert("Invalid data");
+        return;
+    }
+
+    // replacing the placeholder for line break
+    CSV = CSV.replace(/#/g, "\r\n");
+
+    //Generate a file name
+    var fileName = "SkiTrack_GPSData_";
+
+    fileName += date.getFullYear().toString() + "_"
+    			+ date.getMonth().toString() + "_"
+    			+ date.getDate().toString();
+
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName  + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
